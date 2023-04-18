9cb5bf3b914407674fb4cdb2ac25853679d86308
C3sarCruz <106643035+C3sarCruz@users.noreply.github.com>
2023-04-18 12:10:38 -0600

    Initial Commit

---
+++ grafica.js
@@ -0,0 +1,59 @@
+
+ function muestra_GP(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoGP');
+}
+
+ function muestra_AP(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoAP');
+}
+
+ function muestra_EE(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoEE');
+}
+
+ function muestra_M(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoM');
+}
+
+ function muestra_TD(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoTD');
+}
+ function muestra_GV(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_GS('contenidoGV');
+}
